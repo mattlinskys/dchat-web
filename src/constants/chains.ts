243c001b3894b6f107dbd26eea59e55149b29434
Matt <matt@Matts-MacBook-Pro.local>
2022-04-05 22:37:51 +0200

    Update supported chains

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -6,13 +6,13 @@ export const hardhat: Chain = {
 };
 
 export const SUPPORTED_CHAINS = [
-  chain.polygonMainnet,
   chain.polygonTestnetMumbai,
+  // chain.polygonMainnet,
   hardhat,
 ];
 
 export const MULTICALL_ADDRESSES = {
-  [chain.polygonMainnet.id]: "0x275617327c958bD06b5D6b871E7f491D76113dd8",
+  // [chain.polygonMainnet.id]: "0x275617327c958bD06b5D6b871E7f491D76113dd8",
   [chain.polygonTestnetMumbai.id]: "0xe9939e7Ea7D7fb619Ac57f648Da7B1D425832631",
   [hardhat.id]: process.env.REACT_APP_LOCALNODE_MULTICALL_ADDRESS,
 };
